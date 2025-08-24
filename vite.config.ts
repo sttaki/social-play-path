a609a22e23c5e904bd50014bed1b068585791b86
sttaki <131742513+sttaki@users.noreply.github.com>
2025-08-24 09:31:27 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+const repoBase = "/social-play-path/"; // <<< ime tvog repoa na GH Pages
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // Lokalu treba "/" a na GH Pages "/<repo>/" da bi radilo učitavanje asseta/ruta
+  base: mode === "development" ? "/" : repoBase,
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
